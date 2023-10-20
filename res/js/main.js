dc45c58c83632c00c4a9625bc8cd40c1e8a22c30
Your Namec <MamRadZelvyy>
2023-10-20 09:29:06 +0200

    novyclicker

--- res/js/main.js
+++ res/js/main.js
@@ -1,11 +1,39 @@
 const cookie = document.getElementById("cookie");
 const counter = document.getElementById("counter");
+const clickUpgrade = document.getElementById("clickUpgrade");
+const autoclickerUpgrade = document.getElementById("autoclickerUpgrade");
 
 let numberOfCookies = 0;
+let cookieincreaseNumber = 1;
+let cookieAutoclickerincreaseNumber = 0;
+let autoclickerInterval;
+let clickUpgradePrice = 50;
 
 cookie.onclick = () => {
+  numberOfCookies += cookieincreaseNumber;
 
-    numberOfCookies++;
+  counter.innerText = "Sušenky: " + numberOfCookies;
+};
 
+clickUpgrade.onclick = () => {
+  if (numberOfCookies >= clickUpgradePrice) {
+    numberOfCookies -= clickUpgradePrice;
+    clickUpgradePrice *= 2;
+    clickUpgrade.innerText = "Buy click upgrade: {clickupgradeprice}";
     counter.innerText = "Sušenky: " + numberOfCookies;
-}
+    cookieincreaseNumber++;
+  }
+};
+
+autoclickerUpgrade.onclick = () => {
+  if (numberOfCookies >= 100) {
+    numberOfCookies -= 100;
+    counter.innerText = "Sušenky: " + numberOfCookies;
+    cookieAutoclickerincreaseNumber++;
+    clearInterval(autoclickerInterval);
+    autoclickerInterval = setInterval(() => {
+      numberOfCookies += cookieAutoclickerincreaseNumber;
+      counter.innerText = "Sušenky: " + numberOfCookies;
+    }, 1000);
+  }
+};
